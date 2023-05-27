9cfe2bea42976fe2e4c26f9e9abb97bb153506a6
Antoni Przybylik <antoni.przybylik@wp.pl>
2023-05-27 12:51:54 +0200

    Added more tests.

--- tests/algebra_tests.cpp
+++ tests/algebra_tests.cpp
@@ -64,3 +64,48 @@ TEST_CASE("vector")
 		CHECK(float_cmp(v.get_y(), 23.43));
 	}
 }
+
+TEST_CASE("point")
+{
+	SECTION("Default constructor")
+	{
+		Point p;
+
+		CHECK(!p.is_dir());
+		CHECK(float_cmp(p.get_x(), 0.0));
+		CHECK(float_cmp(p.get_y(), 0.0));
+	}
+
+	SECTION("X, Y constructor")
+	{
+		Point p(3.1, 7.81);
+
+		CHECK(!p.is_dir());
+		CHECK(float_cmp(p.get_x(), 3.1));
+		CHECK(float_cmp(p.get_y(), 7.81));
+	}
+}
+
+TEST_CASE("rect")
+{
+	SECTION("Set dimensions 1")
+	{
+		Rect r(1.12, 3.8, 2.22, 3.889);
+
+		CHECK(float_cmp(r.get_pos_x(), 1.12));
+		CHECK(float_cmp(r.get_pos_y(), 3.8));
+		CHECK(float_cmp(r.get_dim_x(), 2.22));
+		CHECK(float_cmp(r.get_dim_y(), 3.889));
+	}
+
+	SECTION("Set dimensions 2")
+	{
+		Rect r(Point(1.12, 3.8),
+		       Vector(2.22, 3.889));
+
+		CHECK(float_cmp(r.get_pos_x(), 1.12));
+		CHECK(float_cmp(r.get_pos_y(), 3.8));
+		CHECK(float_cmp(r.get_dim_x(), 2.22));
+		CHECK(float_cmp(r.get_dim_y(), 3.889));
+	}
+}
